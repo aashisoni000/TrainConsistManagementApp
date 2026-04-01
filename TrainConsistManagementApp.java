d3c1fd0f42cc2aa0e3bfec53bc25a52a3f9dfe65
aashisoni000 <aashi2701soni@gmail.com>
2026-04-01 14:35:41 +0530

    added passenger bogies

--- TrainConsistManagementApp.java
+++ TrainConsistManagementApp.java
@@ -3,10 +3,24 @@ import java.util.List;
 
 public class TrainApp {
     public static void main(String[] args) {
-
+        // UC1: Initialize
         System.out.println("=== Train Consist Management App ===");
-        List<String> trainConsist = new ArrayList<>();
-        System.out.println("Initial Bogie Count: " + trainConsist.size());
-        System.out.println("System initialized and ready.");
+        List<String> passengerBogies = new ArrayList<>();
+        System.out.println("Initial Bogie Count: " + passengerBogies.size());
+
+        // UC2: Add Passenger Bogies (Create)
+        passengerBogies.add("Sleeper");
+        passengerBogies.add("AC Chair");
+        passengerBogies.add("First Class");
+        System.out.println("Bogies added: " + passengerBogies);
+
+        passengerBogies.remove("AC Chair");
+        System.out.println("After removing AC Chair: " + passengerBogies);
+
+        boolean hasSleeper = passengerBogies.contains("Sleeper");
+        System.out.println("Does Sleeper exist? " + hasSleeper);
+
+        System.out.println("Final Train Consist: " + passengerBogies);
+        System.out.println("Program continues...");
     }
 }
