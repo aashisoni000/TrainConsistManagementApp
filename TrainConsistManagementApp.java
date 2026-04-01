07065c3e9658a2ef159e58eb3e98fa8deba12f3a
aashisoni000 <aashi2701soni@gmail.com>
2026-04-01 14:33:40 +0530

    welcome message etc

--- TrainConsistManagementApp.java
+++ TrainConsistManagementApp.java
@@ -4,5 +4,9 @@ import java.util.List;
 public class TrainApp {
     public static void main(String[] args) {
 
+        System.out.println("=== Train Consist Management App ===");
+        List<String> trainConsist = new ArrayList<>();
+        System.out.println("Initial Bogie Count: " + trainConsist.size());
+        System.out.println("System initialized and ready.");
     }
 }
